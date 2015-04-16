706516a6856fed3006c48275b52aefef6e0c4e57
andot <mabingyao@gmail.com>
2015-04-16 08:26:34 +0800

    Import Hprose for Java ME.

---
+++ cdc/src/hprose/common/HproseCallback.java
@@ -0,0 +1,23 @@
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * HproseCallback.java                                    *
+ *                                                        *
+ * hprose callback class for Java.                        *
+ *                                                        *
+ * LastModified: May 16, 2010                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+package hprose.common;
+
+public interface HproseCallback {
+    void handler(Object result, Object[] arguments);
+}
